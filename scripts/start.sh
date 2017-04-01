3d86b95929e65ed13ab1027db6b7fdf1b50fe99f
tchi <tchi@riotgames.com>
2017-04-01 19:13:29 +0900

    Modify start and stop script

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,5 +3,5 @@ if ! [ -d /var/log/riot ];
 then
   sudo mkdir /var/log/riot
 fi
-cd /home/ec2-user/app
-sudo /usr/bin/java -jar -Dserver.port=80 *.jar > /dev/null 2> /dev/null < /dev/null &
+cd /home/ec2-user/$1
+sudo /usr/bin/java -jar -Dserver.port=$2 -Dapplication.name=$1 *.jar > /dev/null 2> /dev/null < /dev/null &
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo /usr/bin/killall java > /dev/null 2> /dev/null < /dev/null
+pid=`ps -eaf | grep application.name=$1 | egrep -v "grep|sudo" | awk '{ print $2 }';`
+sudo /usr/bin/kill -9 $pid > /dev/null 2> /dev/null < /dev/null
