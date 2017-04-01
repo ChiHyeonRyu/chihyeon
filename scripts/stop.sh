0be8af977563c0b45ff6103f86f859cb3f05abd8
tchi <tchi@riotgames.com>
2017-04-01 19:36:18 +0900

    Fix stop script when there is no runninig process

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-pid=`ps -eaf | grep application.name=$1 | egrep -v "grep|sudo" | awk '{ print $2 }';`
-sudo /usr/bin/kill -9 $pid > /dev/null 2> /dev/null < /dev/null
+pid=`ps -eaf | grep application.name=$1 | egrep -v "grep|sudo|jenkins" | awk '{ print $2 }';`
+if [ "$pid" != "" ]; then
+  sudo /usr/bin/kill -9 $pid > /dev/null 2> /dev/null < /dev/null
+fi
