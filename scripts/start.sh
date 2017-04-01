638234c9cc3641916780ffe781109eaccb4cf2dd
tchi <tchi@riotgames.com>
2017-04-01 19:27:15 +0900

    Modify start script to copy the jar to the app dir

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,5 +3,10 @@ if ! [ -d /var/log/riot ];
 then
   sudo mkdir /var/log/riot
 fi
+if ! [ -d /home/ec2-user/$1 ];
+then
+  sudo mkdir /home/ec2-user/$1
+fi
+cp -f $WORKSPACE/target/*.jar /home/ec2-user/$1
 cd /home/ec2-user/$1
 sudo /usr/bin/java -jar -Dserver.port=$2 -Dapplication.name=$1 *.jar > /dev/null 2> /dev/null < /dev/null &
